101b6ab0d99a5987f7d9cf07b6a21d8ca596bb90
UtrechtRoleplay <UtrechtFiveMroleplay@gmail.com>
2025-07-22 17:39:23 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,10 @@
 // Voeg hier je Firebase-config toe
 export const firebaseConfig = {
-   apiKey: "AIzaSyBl1JNsn64WjiPorFpCN48aboNdSzS1Py4",
-    authDomain: "utrechtcontracten.firebaseapp.com",
-    projectId: "utrechtcontracten",
-    storageBucket: "utrechtcontracten.firebasestorage.app",
-    messagingSenderId: "1088300511903",
-    appId: "1:1088300511903:web:f10ba519b201acc237a8aa",
-    measurementId: "G-ZRF8GZDNLV
+  apiKey: "AIzaSyBl1JNsn64WjiPorFpCN48aboNdSzS1Py4",
+  authDomain: "utrechtcontracten.firebaseapp.com",
+  projectId: "utrechtcontracten",
+  storageBucket: "utrechtcontracten.appspot.com",
+  messagingSenderId: "1088300511903",
+  appId: "1:1088300511903:web:f10ba519b201acc237a8aa",
+  measurementId: "G-ZRF8GZDNLV"
 };
