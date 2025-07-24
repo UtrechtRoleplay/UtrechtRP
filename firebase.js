fe12538dbfa2def84f73ca754ac5386e7354b555
UtrechtRoleplay <UtrechtFiveMroleplay@gmail.com>
2025-07-24 17:48:26 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-<script type="module">
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
   import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
   import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
@@ -18,4 +17,3 @@
   const app = initializeApp(firebaseConfig);
   window.auth = getAuth(app);
   window.db = getFirestore(app);
-</script>
