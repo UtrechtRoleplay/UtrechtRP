290fa4ac394fe228294082db5e1e89ba68b22761
UtrechtRoleplay <UtrechtFiveMroleplay@gmail.com>
2025-07-24 17:06:29 +0200

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -4,12 +4,15 @@
   import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
   const firebaseConfig = {
-    apiKey: "JOUW_API_KEY",
-    authDomain: "JOUW_PROJECT_ID.firebaseapp.com",
-    projectId: "JOUW_PROJECT_ID",
-    storageBucket: "JOUW_PROJECT_ID.appspot.com",
-    messagingSenderId: "JOUW_SENDER_ID",
-    appId: "JOUW_APP_ID"
+    export const firebaseConfig = {
+  apiKey: "AIzaSyBl1JNsn64WjiPorFpCN48aboNdSzS1Py4",
+  authDomain: "utrechtcontracten.firebaseapp.com",
+  projectId: "utrechtcontracten",
+  storageBucket: "utrechtcontracten.firebasestorage.app",
+  messagingSenderId: "1088300511903",
+  appId: "1:1088300511903:web:f10ba519b201acc237a8aa",
+  measurementId: "G-ZRF8GZDNLV"
+};
   };
 
   const app = initializeApp(firebaseConfig);
